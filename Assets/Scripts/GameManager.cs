19e85b2c776d15469e0b3694e17bf3b052bb2a93
alan77766 <72923539+alan77766@users.noreply.github.com>
2022-12-05 11:04:44 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject arrowPrefab; //置放Prefab的公開變數
+    public GameObject catfoodPrefab;
+   
+    public GameObject hpGauge;
+
+    void Start ()
+    {
+        InvokeRepeating("MakeArrow", 0, 1.0f);
+        InvokeRepeating("MakeCatfood", 0, 1.0f);
+    }
+
+    
+    void MakeArrow()
+    {
+        int px = Random.Range(-6, 7); // 隨機產生一個-6到6之間的整數
+        // 產生新箭頭，並且設定新箭頭的位置
+        Instantiate(arrowPrefab, new Vector3(px, 7, 0), Quaternion.identity);
+    }
+    void MakeCatfood()
+    {
+        int px = Random.Range(-6, 7); // 隨機產生一個-6到6之間的整數
+        // 產生新箭頭，並且設定新箭頭的位置
+        Instantiate(catfoodPrefab, new Vector3(px, 7, 0), Quaternion.identity);
+    }
+
+
+
+    public void DecreaseHP()
+    {
+        hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+}
